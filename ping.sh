9b48a9c05a4a5e2dce47ddfd486a80f9df53a844
scorevictim <30682499+scorevictim@users.noreply.github.com>
2021-09-16 14:00:20 -0700

    Update ping.sh

--- ping.sh
+++ ping.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+if [ $1 == ""]
+then
+echo "Please at least type a range"
+echo "Example: ./ping.sh 192.168.1"
+
+
+else
 for ip in 'seq 1 254'; do
 ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" $
 done
+fi
